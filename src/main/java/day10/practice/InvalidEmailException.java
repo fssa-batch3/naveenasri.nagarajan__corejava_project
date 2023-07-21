2ae5e4c800b0aa255f6c5b2fd4ecc59fe99c1bd9
Naveena-007 <118426442+Naveena-007@users.noreply.github.com>
2023-07-21 12:28:39 +0530

    initial commit

---
+++ src/main/java/day10/practice/InvalidEmailException.java
@@ -0,0 +1,16 @@
+package day10.practice;
+
+
+
+ class InvalidEmailException {
+	 public static void main(String[] args) {
+			String email = "surya.umapathy@freshworks.com";
+			String regex = "^.*@.*\\..*$";
+			Boolean isMatch = Pattern.matches(regex, email);
+			if (isMatch) {
+				System.out.println("The email address is: Valid");
+			} else {
+				System.out.println("The email address is: Invalid");
+			}
+		}
+}
